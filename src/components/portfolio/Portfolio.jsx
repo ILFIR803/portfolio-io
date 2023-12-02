e455707479ff890046c2d529f9077a85edf19b57
Ilfir <ilfir803@gmail.com>
2023-12-02 12:16:24 +0500

    portfolio ali tmg

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,11 +11,11 @@ function Portfolio() {
             <span className="divider portfolio__divider"></span>
             <div className="portfolio__items">
                <a href="http://ILFIR803.github.io/ubber/" target="_blank" className="portfolio__item"><img src="./img/portfolio/uber.jpg" alt="uber" /></a>
-               <a href="http://ILFIR803.github.io/puls-public/" target="_blank" className="portfolio__item"><img src="./img/portfolio/pulse.jpg" alt="uber" /></a>
-               <a href="http://ILFIR803.github.io/React_Movies/" target="_blank" className="portfolio__item"><img src="./img/portfolio/react.jpg" alt="uber" /></a>
-               <a href="#" className="portfolio__item vertical"><img src="./img/portfolio/plans.jpg" alt="uber" /></a>
-               <a href="http://ILFIR803.github.io/react-foods/" target="_blank" className="portfolio__item"><img src="./img/portfolio/react_food.jpg" alt="uber" /></a>
-               <a href="http://ILFIR803.github.io/react-shop/" target="_blank" className="portfolio__item horizont"><img src="./img/portfolio/react_shop.jpg" alt="uber" /></a>
+               <a href="http://ILFIR803.github.io/puls-public/" target="_blank" className="portfolio__item"><img src="./img/portfolio/pulse.jpg" alt="pulse" /></a>
+               <a href="http://ILFIR803.github.io/React_Movies/" target="_blank" className="portfolio__item"><img src="./img/portfolio/react.jpg" alt="react" /></a>
+               <a href="#" className="portfolio__item vertical"><img src="./img/portfolio/plans.jpg" alt="plans" /></a>
+               <a href="http://ILFIR803.github.io/react-foods/" target="_blank" className="portfolio__item"><img src="./img/portfolio/react_food.jpg" alt="react_food" /></a>
+               <a href="http://ILFIR803.github.io/react-shop/" target="_blank" className="portfolio__item horizont"><img src="./img/portfolio/react_shop.jpg" alt="react_shop" /></a>
 
             </div>
          </div>
